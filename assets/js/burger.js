e2c5f23b3c0be38b06c9cca477150c510641810b
Leno4ka94 <solena677@gmail.com>
2023-10-02 14:37:40 +0300

    fixed some bugs of my site

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,17 +1,23 @@
-const mobileMenu = document.querySelector('.nav');
+const mobileMenu = document.querySelector('nav');
 const menuBtn = document.querySelector('.burger');
+const mobileLinks = document.querySelectorAll('.nav-link');
 const bodyLock = document.querySelector('body');
+
 menuBtn.addEventListener("click", showMenu);
 
 function showMenu() {
   mobileMenu.classList.toggle('active');
   menuBtn.classList.toggle('active');
   bodyLock.classList.toggle('lock');
-};
-
-
-mobileMenu.onclick = function(event) {
-  if (event.target !== button)
-    mobileMenu.style.display = "none";
-    event.stopPropagation();
 }
+
+// Iterate over each mobile link and add a click event listener
+mobileLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+      mobileMenu.classList.remove('active');
+      menuBtn.classList.remove('active');
+      bodyLock.classList.remove('lock');
+  });
+});
+
+
